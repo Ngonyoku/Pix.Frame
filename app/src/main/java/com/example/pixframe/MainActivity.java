f90dc67c9e2ec344c7800d59c9480d64d3867897
Ngonyoku <47557555+Ngonyoku@users.noreply.github.com>
2020-05-18 10:57:31 +0400

    Images can now be Uploaded to Firebase Storage.

--- app/src/main/java/com/example/pixframe/MainActivity.java
+++ app/src/main/java/com/example/pixframe/MainActivity.java
@@ -1,23 +1,41 @@
 package com.example.pixframe;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.pixframe.model.Photos;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int PICK_IMAGE_REQUEST = 1;
+
     private boolean isShown;
     private TextView choose_tv, capture_tv;
     private FloatingActionButton choose_fab, capture_fab;
     private Uri imageUri;
-    public static final int PICK_IMAGE_REQUEST = 1;
+
+    //Firebase
+    private StorageReference storageRef;
+    private DatabaseReference databaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         choose_fab = findViewById(R.id.fab_choose);
         capture_fab = findViewById(R.id.fab_capture);
         isShown = true;
+        storageRef = FirebaseStorage.getInstance().getReference("Photos");
+        databaseRef = FirebaseDatabase.getInstance().getReference("Photos");
 
         choose_fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,10 +76,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void chooseImage() {
+    public void chooseImage() {
         Intent pick = new Intent();
-//        pick.setType("image/*").setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(pick.setType("image/*").setAction(Intent.ACTION_GET_CONTENT), PICK_IMAGE_REQUEST);
+        startActivityForResult(
+                pick.setType("image/*").setAction(Intent.ACTION_GET_CONTENT),
+                PICK_IMAGE_REQUEST);
+        uploadPhoto();
     }
 
     @Override
@@ -73,4 +95,46 @@ public class MainActivity extends AppCompatActivity {
             imageUri = data.getData();
         }
     }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver resolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+
+        return mime.getExtensionFromMimeType(resolver.getType(uri));
+    }
+
+    public void uploadPhoto() {
+        if (imageUri != null) {
+            StorageReference fileRef = storageRef.child(System.currentTimeMillis()
+                    + "."
+                    + getFileExtension(imageUri));
+
+            //Upload the image File to Firebase
+            fileRef.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(MainActivity.this, "Upload was SuccessFul", Toast.LENGTH_SHORT).show();
+                            Photos photo = new Photos("", taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
+
+                            //Set Database Metadata For The Image.
+                            String uploadId = databaseRef.push().getKey();
+                            databaseRef.child(uploadId).setValue(photo);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+
+                }
+            });
+        } else {
+            Toast.makeText(this, "No File Selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/pixframe/model/Photos.java
@@ -0,0 +1,33 @@
+package com.example.pixframe.model;
+
+public class Photos {
+    private String imageUrl, title;
+
+    public Photos() {
+        //The Empty Constructor is Required in Order for the Class to work with Firebase.
+    }
+
+    public Photos(String title, String imageUrl) {
+        this.imageUrl = imageUrl;
+        if (title.trim().equals("")) {
+            title = "My Photo";
+        }
+        this.title = title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
